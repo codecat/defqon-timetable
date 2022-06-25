20d93b7b88be92ba06bc12036b81f01d8cd4ccc9
Melissa Geels <melissa@nimble.tools>
2022-06-25 22:30:21 +0200

    Updates

--- Qtimetable/Program.cs
+++ Qtimetable/Program.cs
@@ -26,7 +26,7 @@ namespace Qtimetable
 		public string emoji = "";
 		public string url = "";
 		public List<Set> sets = new List<Set>();
-		public List<string> faq = new List<string>();
+		public Dictionary<string, string> responses = new Dictionary<string, string>();
 	}
 
 	class Program
@@ -52,6 +52,11 @@ namespace Qtimetable
 		private static DateTime EpochToDate(long epoch)
 		{
 			return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(epoch).ToLocalTime();
+		}
+
+		private static string JsonEncode(string str)
+		{
+			return str.Replace("\\", "\\\\").Replace("\"", "\\\"");
 		}
 
 		static void Main(string[] args)
@@ -98,6 +103,35 @@ namespace Qtimetable
 						// Remove characters that mess with the formatting (remove instead of escape, because it'll make .find easier)
 						title = title.Replace("*", ""); // A*S*Y*S
 
+						// Clarifications
+						if (title.StartsWith("2 ") && title.EndsWith(" 1")) {
+							title += " (Sound Rush & Atmozfears)";
+						}
+						if (title == "Ghost Stories") {
+							title += " (D-Block & S-te-Fan)";
+						}
+						if (title == "De Nachtbrakers") {
+							title += " (Endymion, Degos & Re-Done, Bass Chaserz)";
+						}
+						if (title == "Frenchcore Familia") {
+							title += " (Dr. Peacock, BillX, The Sickest Squad)";
+						}
+
+						// Vertile replaced Zatox
+						if (title == "Zatox") {
+							title = "Vertile";
+						}
+
+						// Crypsis and Donkey Rollers swapped slots
+						if (title == "Crypsis") {
+							title = "Donkey Rollers";
+						} else if (title == "Donkey Rollers") {
+							title = "Crypsis";
+						}
+
+						// Fix encoding
+						title = Encoding.UTF8.GetString(Encoding.Default.GetBytes(title));
+
 						// The MC for a stage has its own "timeslot" prefixed with "hosted by", so we catch that here
 						if (title.ToLower().StartsWith("hosted by ")) {
 							var host = title.Substring("hosted by ".Length);
@@ -133,51 +167,71 @@ namespace Qtimetable
 			}
 
 			// Fill in any missing information
-			GetStage("RED").channel = "989101365345263646";
+			GetStage("RED").channel = "319525278978277407"; // #mainstage
 			GetStage("RED").emoji = "<:dq_red:988093668219031603>";
-			GetStage("RED").url = "https://www.q-dance.com/";
+			GetStage("RED").url = "https://library.q-dance.com/network/live/122407018";
+			GetStage("RED").responses["^\\.(yt|youtube)$"] = ":tv: Select streams might be live on the Q-dance YouTube channel: <https://www.youtube.com/c/qdance/live>";
 
 			GetStage("BLUE").channel = "989101395351318589";
 			GetStage("BLUE").emoji = "<:dq_blue:988094952280055808>";
-			GetStage("BLUE").url = "https://www.q-dance.com/";
+			GetStage("BLUE").url = "https://library.q-dance.com/network/live/122407020";
 
 			GetStage("BLACK").channel = "989101406025822329";
 			GetStage("BLACK").emoji = "<:dq_black:988094951038537778>";
-			GetStage("BLACK").url = "https://www.q-dance.com/";
+			GetStage("BLACK").url = "https://library.q-dance.com/network/live/122407023";
 
 			GetStage("UV").channel = "989101415228141578";
 			GetStage("UV").emoji = "<:dq_uv:988094948199006298>";
-			GetStage("UV").url = "https://www.q-dance.com/";
+			GetStage("UV").url = "https://library.q-dance.com/network/live/122407024";
 
 			GetStage("YELLOW").channel = "989102581232042035";
 			GetStage("YELLOW").emoji = "<:dq_yellow:988094949780246548>";
-			GetStage("YELLOW").url = "https://www.q-dance.com/";
+			GetStage("YELLOW").url = "https://library.q-dance.com/network/live/122407044";
 
 			GetStage("INDIGO").channel = "989102615843450880";
 			GetStage("INDIGO").emoji = "<:dq_indigo:988094943790792724>";
-			GetStage("INDIGO").url = "https://www.q-dance.com/";
+			GetStage("INDIGO").url = "https://library.q-dance.com/network/live/122407026";
 
 			GetStage("MAGENTA").channel = "989102625570041866";
 			GetStage("MAGENTA").emoji = "<:dq_magenta:988094945057452203>";
-			GetStage("MAGENTA").url = "https://www.q-dance.com/";
+			GetStage("MAGENTA").url = "https://library.q-dance.com/network/live/122407040";
 
 			GetStage("GOLD").channel = "989102635187576843";
 			GetStage("GOLD").emoji = "<:dq_gold:988094953903231036>";
-			GetStage("GOLD").url = "https://www.q-dance.com/";
+			GetStage("GOLD").url = "https://library.q-dance.com/network/live/122407042";
 
 			GetStage("SILVER").channel = "989102642808631357";
 			GetStage("SILVER").emoji = "<:dq_silver:988094946756141156>";
-			GetStage("SILVER").url = "https://www.q-dance.com/";
+			GetStage("SILVER").url = "https://library.q-dance.com/network/live/122407043";
 
 			GetStage("PURPLE").channel = "989102651864154122";
 			GetStage("PURPLE").emoji = "<:dq_purple:988094360006574103>";
-			GetStage("PURPLE").url = "https://www.q-dance.com/";
+			GetStage("PURPLE").url = "https://library.q-dance.com/network/live/122407046";
 
 			// Remove stages that don't have a channel set
 			for (int i = Stages.Count - 1; i >= 0; i--) {
 				if (Stages[i].channel == "") {
 					Stages.RemoveAt(i);
 				}
+			}
+
+			// Add common responses to all stages
+			foreach (var stage in Stages) {
+				stage.responses["^\\.(url|stream|link|watch)$"] =
+					":tv: Tune in to the livestream here: **<" + stage.url + ">**";
+				stage.responses["^\\.(recording|rec)$"] =
+					"<:police:359836811285102593> Recording is considered piracy which is against the rules. Do not ask for or " +
+					"share recordings!";
+				stage.responses["^\\.(tickets|buy|paid)$"] =
+					":money_with_wings: This is a **paid** livestream. Buy tickets here: " +
+					"**<https://www.q-dance.com/en/events/defqon-1/defqon-1-weekend-festival-2022/livestream>**";
+				stage.responses["^\\.(hidechat|removechat|fuckchat)$"] =
+					":thinking: Press F12 and paste this into the console to hide the chat on live.q-dance.com: " +
+					"```sc=document.getElementById(\"scrollContainer\");sc.classList.remove(\"col-l--9\");sc.classList.remove(\"col-m--8\");" +
+					"sc.classList.add(\"col-l--12\");sc.classList.remove(\"col-m--12\");``` :woman_tipping_hand: You can also use this userscript: " +
+					"<https://greasyfork.org/en/scripts/446916-hide-q-dance-chat>";
+				stage.responses["^\\.(mc|mcs)$"] =
+					":microphone2: The MC(s) on this stage: **" + stage.mc + "**";
 			}
 
 			// Sort stages
@@ -202,22 +256,17 @@ namespace Qtimetable
 
 					writer.WriteLine("\t{");
 					writer.WriteLine("\t\t\"stage\": \"{0}\",", stage.stage);
-					writer.WriteLine("\t\t\"mc\": \"{0}\",", stage.mc);
 					writer.WriteLine("\t\t\"channel\": \"{0}\",", stage.channel);
 					writer.WriteLine("\t\t\"emoji\": \"{0}\",", stage.emoji);
 					writer.WriteLine("\t\t\"url\": \"{0}\",", stage.url);
+					writer.WriteLine("\t\t\"streamdelay\":2,");
 					writer.WriteLine("\t\t\"sets\": [");
 					for (int i = 0; i < stage.sets.Count; i++) {
 						var set = stage.sets[i];
-						var setName = set.name.Replace("\"", "\\\"");
-
-						// Fix encoding
-						setName = Encoding.UTF8.GetString(Encoding.Default.GetBytes(setName));
-
-						if (setName == "") {
+						if (set.name == "") {
 							writer.Write("\t\t\t\t[ {0}, {1}, {2}, {3}, {4} ]", set.dateTime.Year, set.dateTime.Month, set.dateTime.Day, set.dateTime.Hour, set.dateTime.Minute);
 						} else {
-							writer.Write("\t\t\t[ {0}, {1}, {2}, {3}, {4}, \"{5}\" ]", set.dateTime.Year, set.dateTime.Month, set.dateTime.Day, set.dateTime.Hour, set.dateTime.Minute, setName);
+							writer.Write("\t\t\t[ {0}, {1}, {2}, {3}, {4}, \"{5}\" ]", set.dateTime.Year, set.dateTime.Month, set.dateTime.Day, set.dateTime.Hour, set.dateTime.Minute, JsonEncode(set.name));
 						}
 						if (i == stage.sets.Count - 1) {
 							writer.WriteLine();
@@ -226,16 +275,17 @@ namespace Qtimetable
 						}
 					}
 					writer.WriteLine("\t\t],");
-					writer.WriteLine("\t\t\"faq\": [");
-					for (int i = 0; i < stage.faq.Count; i++) {
-						writer.Write("\t\t\t\"{0}\"", stage.faq[i]);
-						if (i == stage.faq.Count - 1) {
+					writer.WriteLine("\t\t\"responses\": {");
+					for (int i = 0; i < stage.responses.Count; i++) {
+						var pair = stage.responses.ElementAt(i);
+						writer.Write("\t\t\t\"{0}\":\"{1}\"", JsonEncode(pair.Key), JsonEncode(pair.Value));
+						if (i == stage.responses.Count - 1) {
 							writer.WriteLine();
 						} else {
 							writer.WriteLine(",");
 						}
 					}
-					writer.WriteLine("\t\t]");
+					writer.WriteLine("\t\t}");
 					writer.Write("\t}");
 
 					if (j == Stages.Count - 1) {
@@ -245,6 +295,34 @@ namespace Qtimetable
 					}
 				}
 				writer.WriteLine("]");
+			}
+
+			// Write Markdown table to file
+			if (File.Exists("Defqon2022.md")) {
+				File.Delete("Defqon2022.md");
+			}
+			using (var writer = new StreamWriter("Defqon2022.md", false, Encoding.UTF8)) {
+				writer.NewLine = "\n";
+
+				writer.WriteLine("# Schedule");
+				writer.WriteLine("This is the full timetable for the Defqon 2022 livestreams. I will keep it updated the best I can.");
+				writer.WriteLine();
+
+				foreach (var stage in Stages) {
+					writer.WriteLine("## " + stage.stage);
+					writer.WriteLine("Watch live: {0}", stage.url);
+					writer.WriteLine();
+					writer.WriteLine("Day | Time ([CEST](https://time.is/CEST)) | Set");
+					writer.WriteLine("--|--|--");
+					for (int i = 0; i < stage.sets.Count; i++) {
+						var set = stage.sets[i];
+						if (set.name == "" && i == stage.sets.Count - 1) {
+							continue;
+						}
+						writer.WriteLine("{0} | {1} | {2}", set.dateTime.ToString("dddd"), set.dateTime.ToString("HH:mm"), set.name);
+					}
+					writer.WriteLine();
+				}
 			}
 
 			Console.ReadKey();
